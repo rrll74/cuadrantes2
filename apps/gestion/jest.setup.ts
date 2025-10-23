5bfdf2b598e8c46675b68dc83f52c74e182c999d
Ramon Luque <rrl.luque@gmail.com>
2025-10-23 12:43:55 +0200

    Persisten los mismos errores y ahora, adicionalmente no funcional el comando npm run test:apie2e

--- apps/gestion/jest.setup.ts
+++ apps/gestion/jest.setup.ts
@@ -1,6 +1,6 @@
 // jest.setup.ts
+// Este archivo se ejecuta automáticamente antes de cada test.
 
 // Importa los matchers personalizados de jest-dom (ej: .toBeInTheDocument())
-// para que estén disponibles en todos los ficheros de test.
-// Esto resuelve los errores de TypeScript y configura Jest.
+// para que estén disponibles en todos los archivos de test.
 import "@testing-library/jest-dom";
