16f108811c7f5d6205b91f809c80741ea1fd5b7f
Ramón Luque <rrl.luque@gmail.com>
2026-03-02 20:41:19 +0100

    Pequeña modificación para descarga de órdenes de trabajo generadas para que el texto del nombre del fichero aparezca en mayúsculas.

--- apps/gestion/src/lib/parte-trabajo-pdf.ts
+++ apps/gestion/src/lib/parte-trabajo-pdf.ts
@@ -92,7 +92,7 @@ export const generateParteTrabajoPdfFromData = async (data: ParteTrabajo) => {
       // Guardar cada PDF con nombre único
       //const timestamp = new Date().getTime();
       //const fileName = `Parte-Trabajo-${numeroDocumentoConSufijo}-${timestamp}.pdf`;
-      const fileName = `E-${numeroDocumentoConSufijo} ${servicio}.pdf`;
+      const fileName = `E-${numeroDocumentoConSufijo} ${servicio.toLocaleUpperCase()}.pdf`;
       pdf.save(fileName);
 
       // Pequeña pausa entre PDFs para evitar problemas de descarga
